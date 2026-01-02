4b2ce54e8ec869f63398c58d5f5eb733ee5a2ff7
Asilbek2706 <asilbekkaromatov2@gmail.com>
2026-01-03 00:56:18 +0500

    addition examples added

--- maps-and-sets/app.js
+++ maps-and-sets/app.js
@@ -1,93 +1,120 @@
 // maps
-const myMap = new Map()
+// const myMap = new Map([
+//     ['firstName', 'Asilbek'],
+//     ['lastName', 'Karomatov'],
+//     ['age', 20]
+// ])
+// const myMap = new Map()
+//     .set('name', 'Asilbek')
+//     .set('job', 'Dev')
+//     .set('isOnline', true);
+// console.log(myMap)
+//
+// console.log(myMap.get('firstname'))
+//
+// myMap.set('job', 'Frontend Developer')
+// console.log(myMap.get('job'))
+//
+// console.log(myMap.has('firstname'))
+//
+// console.log(myMap.size)
+//
+// myMap.delete('lastname')
+// console.log(myMap.size)
+// console.log(myMap)
+//
+//
+// const newMap = new Map()
+// newMap['firstname'] = 'Asilbek'
+// newMap['lastname'] = 'Karomatov'
+//
+// console.log(newMap)
+// console.log(newMap.has('firstname'))
+// newMap.delete('lastname')
+// console.log(newMap)
+//
+//
+// //sets
+// const mySet = new Set()
+// mySet.add(10)
+// mySet.add(15)
+// mySet.add(15)
+// mySet.add("Asilbek")
+// const person = {firstname: 'Asilbek', lastname: 'Karomatov'}
+// mySet.add(person)
+//
+// console.log(mySet)
+//
+// mySet.add({firstname: 'Asilbek', lastname: 'Karomatov'})
+//
+// console.log(mySet.has(10))
+// console.log(mySet.has(11))
+// console.log(mySet.has(person))
+//
+// console.log(mySet.size)
+//
+// mySet.delete(10)
+// console.log(mySet.has(10))
+//
+// const sonlar = [1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 5, 7, 8, 8, 9]
+// const newSet = [...new Set(sonlar)]
+// console.log(newSet)
+//
+// const userIds = [101, 102, 101, 105, 102, 110, 105];
+//
+// const uniqueIds = [...new Set(userIds)]
+// console.log(uniqueIds);
+//
+// const dictionary = new Map()
+// dictionary.set('olma', 'apple')
+// dictionary.set('banan', 'banana')
+//
+// console.log(dictionary.has('cherry'))
+// console.log(dictionary.size)
+// console.log(dictionary)
+//
+// const aMap = new Map();
+// const obj = { id: 1 };
+//
+// aMap.set(obj, "Admin");
+//
+// console.log(aMap.has({ id: 1 }));
+// console.log(aMap.has(obj));
+//
+// const mevalar = ['Olma', 'Banan', 'Uzum'];
+//
+// for (const meva of mevalar) {
+//     console.log(meva);
+// }
+//
+// const prices = new Map([
+//     ['Bread', 2],
+//     ['Milk', 5],
+//     ['Cheese', 15]
+// ]);
+//
+// for (const [product, price] of prices) {
+//     console.log(`${product}: ${price} $`);
+// }
 
-myMap.set('firstname', 'Asilbek')
-myMap.set('lastname', 'Karomatov')
-myMap.set('age', 20)
+const personObj = { name: 'Asilbek', age: 20 };
+const myMap = new Map(Object.entries(personObj));
 
-console.log(myMap.get('firstname'))
+console.log(myMap.get('name'))
 
-myMap.set('job', 'Frontend Developer')
-console.log(myMap.get('job'))
-
-console.log(myMap.has('firstname'))
-
-console.log(myMap.size)
-
-myMap.delete('lastname')
-console.log(myMap.size)
-console.log(myMap)
+const colors = new Set()
+    .add('qizil')
+    .add('yashil')
+    .add('koʻk');
+console.log(colors)
 
 
-const newMap = new Map()
-newMap['firstname'] = 'Asilbek'
-newMap['lastname'] = 'Karomatov'
+const oldArray = ['Ali', 'Vali', 'Soli', 'Ali', 'Gani', 'Vali'];
+const cleanArray = [...new Set(oldArray)];
 
-console.log(newMap)
-console.log(newMap.has('firstname'))
-newMap.delete('lastname')
-console.log(newMap)
+console.log(cleanArray);
 
+const lotereyaSonlari = [10, 25, 10, 99, 25, 30];
+const unikalSonlar = [...new Set(lotereyaSonlari)];
 
-//sets
-const mySet = new Set()
-mySet.add(10)
-mySet.add(15)
-mySet.add(15)
-mySet.add("Asilbek")
-const person = {firstname: 'Asilbek', lastname: 'Karomatov'}
-mySet.add(person)
-
-console.log(mySet)
-
-mySet.add({firstname: 'Asilbek', lastname: 'Karomatov'})
-
-console.log(mySet.has(10))
-console.log(mySet.has(11))
-console.log(mySet.has(person))
-
-console.log(mySet.size)
-
-mySet.delete(10)
-console.log(mySet.has(10))
-
-const sonlar = [1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 5, 7, 8, 8, 9]
-const newSet = [...new Set(sonlar)]
-console.log(newSet)
-
-const userIds = [101, 102, 101, 105, 102, 110, 105];
-
-const uniqueIds = [...new Set(userIds)]
-console.log(uniqueIds);
-
-const dictionary = new Map()
-dictionary.set('olma', 'apple')
-dictionary.set('banan', 'banana')
-
-console.log(dictionary.has('cherry'))
-console.log(dictionary.size)
-console.log(dictionary)
-
-const aMap = new Map();
-const obj = { id: 1 };
-
-aMap.set(obj, "Admin");
-
-console.log(aMap.has({ id: 1 }));
-console.log(aMap.has(obj));
-
-const mevalar = ['Olma', 'Banan', 'Uzum'];
-
-for (const meva of mevalar) {
-    console.log(meva);
-}
-
-const prices = new Map([
-    ['Bread', 2],
-    ['Milk', 5],
-    ['Cheese', 15]
-]);
-
-for (const [product, price] of prices) {
-    console.log(`${product}: ${price} $`);
-}
+console.log(unikalSonlar);
